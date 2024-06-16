e87940679f1455e0e72c1179e6d584bc82fe17b7
Kacper <116844039+prattling-pate@users.noreply.github.com>
2024-06-17 00:32:25 +0100

    Update logger.py

--- utility/logger.py
+++ utility/logger.py
@@ -4,13 +4,13 @@ from datetime import datetime
 
 class Logger:
     def __init__(self, file_path : str):
-        self.__file = File(file_path)
+        self._file = File(file_path)
     
     def log(self, to_log : str):
         now = datetime.now()
         now_str = now.strftime("%d/%m/%Y %H:%M:%S")
         message : str = f"<Info> {now_str} :" + to_log
-        self.__file.modify_contents(Append(), -1 , message)
+        self._file.modify_contents(Append(), -1 , message)
 
     def write_log(self):
-        self.__file.write_to_file()
+        self._file.write_to_file()
