6c0b6c93626dbe013b0d615d9ff99d77a87de196
Kacper <116844039+prattling-pate@users.noreply.github.com>
2024-06-17 00:33:36 +0100

    Update file.py

--- logic/file.py
+++ logic/file.py
@@ -4,38 +4,38 @@ class File:
     """Class representing the abstraction of a file in the text editor"""
 
     def __init__(self, file_path: str):
-        self.__path : str = file_path
-        self.__contents : list[str] = self.__fetch_file_contents()
+        self._path : str = file_path
+        self._contents : list[str] = self._fetch_file_contents()
 
-    def __fetch_file_contents(self) -> list[str]:
+    def _fetch_file_contents(self) -> list[str]:
         """Returns the contents of the file represented in object as a list of strings"""
         file_contents: list[str] = ["\n"]
         try:
-            with open(self.__path, "r") as file:
+            with open(self._path, "r") as file:
                 file_contents = file.readlines()
                 # prevents IndexError when file has nothing inside of it
                 if (len(file_contents) == 0) :
                     file_contents.append("\n")
         except IOError:
-            # (f"cannot write to file @{self.__path}, creating file")
-            f = open(self.__path, "x")
+            # (f"cannot write to file @{self._path}, creating file")
+            f = open(self._path, "x")
             f.write(file_contents[0])
             f.close()
         return file_contents
 
-    def write_to_file(self):
+    def write_to_file(self) -> None:
         """Writes current contents to file on disk"""
-        with open(self.__path, "w") as file:
-            file.writelines(self.__contents)
+        with open(self._path, "w") as file:
+            file.writelines(self._contents)
 
     def modify_contents(self,command : Command, line : int , *args) -> None:
         """
         Modifies the contents of the file given a line number and string index
         """
-        self.__contents[line]=command.execute_command(self.__contents[line], *args)
+        self._contents[line]=command.execute_command(self._contents[line], *args)
 
-    def get_line(self, line : int):
-        return self.__contents[line]
+    def get_line(self, line : int) -> str:
+        return self._contents[line]
 
     def get_file_contents(self) -> list[str]:
-        return self.__contents
+        return self._contents
